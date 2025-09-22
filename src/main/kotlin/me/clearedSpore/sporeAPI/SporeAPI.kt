7d90880ddcbbece6dbc4494e7e34ffd2ad7fda06
Clearedspore <spore1325@gmail.com>
2025-09-22 16:39:42 +0200

    Repo upload

---
+++ src/main/kotlin/me/clearedSpore/sporeAPI/SporeAPI.kt
@@ -0,0 +1,12 @@
+package me.clearedSpore.sporeAPI
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SporeAPI : JavaPlugin() {
+
+    override fun onEnable() {
+    }
+
+    override fun onDisable() {
+    }
+}
