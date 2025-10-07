5651d965472ba36c820384b5ebe6926f18861b95
Clearedspore <spore1325@gmail.com>
2025-10-07 16:01:49 +0200

    feat: Updated StringUtil

--- src/main/kotlin/me/clearedSpore/sporeAPI/util/StringUtil.kt
+++ src/main/kotlin/me/clearedSpore/sporeAPI/util/StringUtil.kt
@@ -5,12 +5,15 @@ package me.clearedSpore.sporeAPI.util
 
 object StringUtil {
 
-    fun String.capitalizeFirstLetter(input: String?): String? {
+    fun String.capitalizeFirstLetter(): String? {
+        val input = this
         if (input.isNullOrEmpty()) return input
         val lower = input.lowercase()
         return lower.replaceFirstChar(Char::uppercaseChar)
     }
 
-    fun String.joinWithSpaces(vararg parts: String?): String =
-        parts.filterNotNull().joinToString(" ")
+    fun String.joinWithSpaces(vararg parts: String?): String {
+        return listOf(this, *parts).filterNotNull().joinToString(" ")
+    }
+
 }
