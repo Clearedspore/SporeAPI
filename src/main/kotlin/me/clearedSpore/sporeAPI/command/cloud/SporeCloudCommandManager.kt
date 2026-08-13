1ae6b01f7b3f053d8e7e15734b70d79ea17061d7
Clearedspore <151540868+Clearedspore@users.noreply.github.com>
2026-08-13 16:11:22 +0200

    feat: Change cloud player handler

--- src/main/kotlin/me/clearedSpore/sporeAPI/command/cloud/SporeCloudCommandManager.kt
+++ src/main/kotlin/me/clearedSpore/sporeAPI/command/cloud/SporeCloudCommandManager.kt
@@ -2,13 +2,16 @@ package me.clearedSpore.sporeAPI.command.cloud
 
 import io.papermc.paper.command.brigadier.CommandSourceStack
 import me.clearedSpore.sporeAPI.SporePlugin
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 import org.incendo.cloud.annotations.AnnotationParser
+import org.incendo.cloud.exception.InjectionException
 import org.incendo.cloud.execution.ExecutionCoordinator
+import org.incendo.cloud.injection.ParameterInjector
 import org.incendo.cloud.paper.PaperCommandManager
 
 // Copyright (c) 2025 ClearedSpore
 // Licensed under the MIT License. See LICENSE file in the project root for details.
-
 
 class SporeCloudCommandManager(
     plugin: SporePlugin
@@ -22,6 +25,22 @@ class SporeCloudCommandManager(
         manager,
         CommandSourceStack::class.java
     )
+
+    init {
+        manager.parameterInjectorRegistry().registerInjector(
+            CommandSender::class.java,
+            ParameterInjector { context, _ -> context.sender().sender }
+        )
+
+        manager.parameterInjectorRegistry().registerInjector(
+            Player::class.java,
+            ParameterInjector { context, _ -> context.sender().sender as? Player }
+        )
+
+        manager.exceptionController().registerHandler(InjectionException::class.java) { ctx ->
+            ctx.context().sender().sender.sendMessage("§cOnly players can run this command.")
+        }
+    }
 
     fun register(instance: Any) {
         annotationParser.parse(instance)
